c9bab2b17fe656a8a46b39dc67bd1fe269827f2e
boscoybarra <bosco@ybarra.io>
2019-12-06 23:21:42 +0100

    100daysofswiftui final day of basics

---
+++ Day 12/Optionals.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Optionals
+
+//An optional is trying to answer the question “what if our variable doesn’t have a value?” Swift wants to make sure all our programs are as safe as possible,
+
+
+//Handling missing data
+var age: Int? = nil
+
+//That doesn’t hold any number – it holds nothing. But if we later learn that age, we can use it:
+age = 38
+
+
