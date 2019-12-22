ddf04a4098343fd46779f2faeb60ef9d2d51662c
boscoybarra <bosco@ybarra.io>
2019-12-22 23:53:03 +0100

    Working with Create ML App to train ML models in app

--- Project 4 - BetterRest/BetterRest/BetterRest/ContentView.swift
+++ Project 4 - BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -10,10 +10,107 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date()
+//    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
+        
+        NavigationView {
+            Form {
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+// If you try compiling that code you’ll see it fails, and the reason is that we’re accessing one property from inside another – Swift doesn’t know which order the properties will be created in, so this isn’t allowed.
+
+// The fix here is simple: we can make defaultWakeTime a static variable, which means it belongs to the ContentView struct itself rather than a single instance of that struct. This in turn means defaultWakeTime can be read whenever we want, because it doesn’t rely on the existence of any other properties.
+
+// So, change the property definition to this:
+    
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
+    func calculateBedtime() {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+           let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is…"
+            
+        } catch {
+            // something went wrong!
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+        
+    }
+}
         
         // when you create a new Date instance it will be set to the current date and time
 //        let now = Date()
@@ -24,24 +121,23 @@ struct ContentView: View {
         // create a range from those two
 //        let range = now ... tomorrow
         
-        VStack {
-        
-            Form {
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
-                }
-            }
-            
-            Form {
+//        VStack {
+//
+//            Form {
+//                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+//                    Text("\(sleepAmount, specifier: "%g") hours")
+//                }
+//            }
+//
+//            Form {
 //                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 
 //              Simpler solution
-                DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+//                DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
 //                .labelsHidden()
-            }
-        }
-    }
-}
+//            }
+//        }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
