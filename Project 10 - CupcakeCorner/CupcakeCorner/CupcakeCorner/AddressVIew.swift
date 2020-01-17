d70f24fec1b6773d6fdb5fa2e506b4fbed0b52e3
Bosco <bosco@Mac-mini-de-Bosco.local>
2020-01-17 17:23:37 +0100

    DAY 50

---
+++ Project 10 - CupcakeCorner/CupcakeCorner/CupcakeCorner/AddressVIew.swift
@@ -0,0 +1,38 @@
+//
+//  Address.swift
+//  CupcakeCorner
+//
+//  Created by Bosco on 17/01/2020.
+//  Copyright © 2020 Bosco Ybarra. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink(destination: CheckoutView(order: order)) {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationBarTitle("Delivery details", displayMode: .inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
---
+++ Project 10 - CupcakeCorner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Bosco on 17/01/2020.
+//  Copyright © 2020 Bosco Ybarra. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
