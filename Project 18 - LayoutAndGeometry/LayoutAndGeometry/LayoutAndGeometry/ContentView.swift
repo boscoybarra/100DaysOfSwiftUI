15916a7ef2b7cb47830b970ecf8171061310f5fc
Bosco <bosco@Mac-mini-de-Bosco.local>
2020-03-29 17:20:36 +0100

    DAY 93

--- Project 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Project 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,50 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-//        HStack {
-//            VStack {
-//                Text("@boscoy_")
-//                Image("bosco-ybarra")
-//                    .resizable()
-//                    .frame(width: 64, height: 64)
-//            }
-//
-//            VStack {
-//                Text("Full name:")
-//                Text("BOSCO YBARRA")
-//                    .font(.largeTitle)
-//            }
-//        }
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@boscoy_")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("bosco-ybarra")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("Bosco Ybarra")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - 150) / 2)
             }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
-
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
